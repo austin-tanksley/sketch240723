1296af6ea291f166a617417facb8796243ca2059
Austin Tanksley <58612782+austin-tanksley@users.noreply.github.com>
2024-07-23 12:08:15 -0500

    test pig

---
+++ sketch.js
@@ -0,0 +1,25 @@
+let porky;
+let options = {
+  normalize: true,
+  successCallback: null,
+  failureCallback: console.log("Not Loaded"),
+  fileType: '.obj'
+};
+
+function preload() {
+  porky = loadModel('/assets/porky.obj', options);
+}
+
+function setup(){
+    createCanvas(800,800, WEBGL);
+    normalMaterial();
+}
+
+function draw() {
+    background(255, 188, 66);
+    orbitControl();
+    translate(0,150);
+    model(porky);
+
+
+}
