932b20a0cd3caabca83205dfe571db5672a6b9ba
Austin Tanksley <58612782+austin-tanksley@users.noreply.github.com>
2024-07-23 15:44:25 -0500

    change color

--- sketch.js
+++ sketch.js
@@ -1,6 +1,6 @@
 let porky;
 let options = {
-  normalize: true,
+  normalize: false,
   successCallback: null,
   failureCallback: console.log("Not Loaded"),
   fileType: '.obj'
@@ -12,14 +12,21 @@ function preload() {
 
 function setup(){
     createCanvas(1920,800, WEBGL);
-    normalMaterial();
     frameRate(60);
 }
 
 function draw() {
     background(0);
     orbitControl();
+    
+    ambientLight(10, 0, 10,2);
+    let c = color(251, 255, 255);
+    directionalLight(c, 0, 1, 0);
 
+    c = color(255, 32, 110);
+    directionalLight(c, 0, -1, 0);
+    noStroke();
+    
     push();
     translate(0,150);
     rotateY(.02*frameCount); 
