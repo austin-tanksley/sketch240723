646fae1d79d40a2ec5b1cec99fcbeff76ad8fb1a
Austin Tanksley <58612782+austin-tanksley@users.noreply.github.com>
2024-07-24 15:11:41 -0500

    added loading screen

--- sketch.js
+++ sketch.js
@@ -1,4 +1,9 @@
 let porky;
+let font;
+let loaded = false;
+function toggle(){
+  loaded = true;
+}
 let options = {
   normalize: false,
   successCallback: null,
@@ -6,19 +11,34 @@ let options = {
   fileType: '.obj'
 };
 
+
 function preload() {
-  porky = loadModel('./assets/porky.obj', options);
+  porky = loadModel('./assets/porky.obj', options,);
+  font = loadFont('./assets/SuisseIntl-Regular-WebM.otf');
 }
 
 function setup(){
-    createCanvas(1920,800, WEBGL);
+    createCanvas(windowWidth,800, WEBGL);
     frameRate(60);
+      if (!loaded){
+        fill(220);
+        textFont(font);
+        textAlign(CENTER, CENTER);
+        textSize(24);
+        textStyle(BOLD)
+        text("this could be some sweet graphics loading lots of bits.",0 , 0);
+    }
 }
 
 function draw() {
+
+
     background(0);
+
     orbitControl();
     
+    
+
     ambientLight(10, 0, 10,2);
     let c = color(251, 220, 255);
     directionalLight(c, 1, 0, 0);
@@ -26,11 +46,16 @@ function draw() {
     c = color(255, 32, 110);
     directionalLight(c, -1, 0, 0);
     noStroke();
-    
+    toggle();
+  
     push();
     translate(0,150);
     rotateY(.02*frameCount); 
     model(porky);
     pop();
+
+    toggle();
+    
+
     
 }
